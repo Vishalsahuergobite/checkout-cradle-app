5aaf99deac948add5af818b7fe6882b8b8a5b126
Vishal Sahu <vishal.sahu@cradlewise.com>
2023-05-18 12:26:31 +0530

    extention changes

--- extensions/checkout-cradle-app/src/index.jsx
+++ extensions/checkout-cradle-app/src/index.jsx
@@ -19,38 +19,42 @@ import {
 
 render('Checkout::Dynamic::Render', () => <App1 />);
 
-// const PRODUCT_VARIANTS_DATA1 = [
-//   {
-//     id: "gid://shopify/ProductVariant/40439056433199",
-//     img: "https://assets.cradlewise.com/images/products/fitted-sheet/Cradlewise-Crib-Fitted-Sheet-6-rotated.jpeg",
-//     title: "Cradlewise Fitted sheet",
-//     price: "$24.99",
-//   },
-  
-//   {
-//     id: "gid://shopify/ProductVariant/40614492209199",
-//     img: "https://assets.cradlewise.com/images/products/mattress-cover/PXL_20220304_110704438.jpg",
-//     title: "Cradlewise Mattress Cover",
-//     price: "$45.00",
-//   },
-// ];
 
+// for Live 
 const PRODUCT_VARIANTS_DATA1 = [
   {
-    id: "gid://shopify/ProductVariant/44574727471380",
+    id: "gid://shopify/ProductVariant/40439056433199",
     img: "https://assets.cradlewise.com/images/products/fitted-sheet/Cradlewise-Crib-Fitted-Sheet-6-rotated.jpeg",
     title: "Cradlewise Fitted sheet",
     price: "$24.99",
   },
-
+  
   {
-    id: "gid://shopify/ProductVariant/44574399398164",
+    id: "gid://shopify/ProductVariant/40614492209199",
     img: "https://assets.cradlewise.com/images/products/mattress-cover/PXL_20220304_110704438.jpg",
     title: "Cradlewise Mattress Cover",
     price: "$45.00",
   },
-
 ];
+
+
+// for Testing 
+// const PRODUCT_VARIANTS_DATA1 = [
+//   {
+//     id: "gid://shopify/ProductVariant/44574727471380",
+//     img: "https://assets.cradlewise.com/images/products/fitted-sheet/Cradlewise-Crib-Fitted-Sheet-6-rotated.jpeg",
+//     title: "Cradlewise Fitted sheet",
+//     price: "$24.99",
+//   },
+
+//   {
+//     id: "gid://shopify/ProductVariant/44574399398164",
+//     img: "https://assets.cradlewise.com/images/products/mattress-cover/PXL_20220304_110704438.jpg",
+//     title: "Cradlewise Mattress Cover",
+//     price: "$45.00",
+//   },
+
+// ];
 
 
 function App1() {
@@ -67,14 +71,29 @@ function App1() {
   const [ids2, setIds2] = useState("");
 
   // for live 
+  useEffect(() => {
+    if (cartLine.length > 1) {
+      cartLine.map((ele, i) => {
+        if (ele.merchandise.id == 'gid://shopify/ProductVariant/40439056433199') {
+          setIds1(ele.merchandise.id)
+        } 
+
+        if (ele.merchandise.id == 'gid://shopify/ProductVariant/40614492209199') {
+          setIds2(ele.merchandise.id)
+        }
+      })
+    }
+  }, [cartLine])
+
+
+  // for Testing 
   // useEffect(() => {
   //   if (cartLine.length > 1) {
   //     cartLine.map((ele, i) => {
-  //       if (ele.merchandise.id == 'gid://shopify/ProductVariant/40439056433199') {
+  //       if (ele.merchandise.id == 'gid://shopify/ProductVariant/44574727471380') {
   //         setIds1(ele.merchandise.id)
-  //       } 
-
-  //       if (ele.merchandise.id == 'gid://shopify/ProductVariant/40614492209199') {
+  //       }
+  //       if (ele.merchandise.id == 'gid://shopify/ProductVariant/44574399398164') {
   //         setIds2(ele.merchandise.id)
   //       }
   //     })
@@ -82,41 +101,21 @@ function App1() {
   // }, [cartLine])
 
 
-
   useEffect(() => {
-    if (cartLine.length > 1) {
-      cartLine.map((ele, i) => {
-        if (ele.merchandise.id == 'gid://shopify/ProductVariant/44574727471380') {
-          setIds1(ele.merchandise.id)
-        }
-
-        if (ele.merchandise.id == 'gid://shopify/ProductVariant/44574399398164') {
-          setIds2(ele.merchandise.id)
-        }
-      })
-    }
-  }, [cartLine])
-
-  // On initial load, fetch the product variants
-  useEffect(() => {
-    // Set the loading state to show some UI if you're waiting
     setLoading(true);
-    // If you're making a network request, then replace the following code with the HTTP call
-    // If you don't need to make a network request, then you can remove this `useEffect`
     new Promise((resolve) => {
       setTimeout(() => {
         resolve(PRODUCT_VARIANTS_DATA1);
       }, 800);
     })
       .then((result) => {
-        // Set the "products" array so that you can reference the array items
         setProducts(result);
       })
       .catch((error) => console.error(error))
       .finally(() => setLoading(false));
   }, []);
 
-  // If an offer is added and an error occurs, then show some error feedback using a banner
+
   useEffect(() => {
     if (showError) {
       const timer = setTimeout(() => setShowError(false), 3000);
@@ -124,11 +123,9 @@ function App1() {
     }
   }, [showError]);
 
-  // Access the current cart lines and subscribe to changes
+
   const lines = useCartLines();
 
-  // Show a loading UI if you're waiting for product variant data
-  // Use Skeleton components to keep placement from shifting when content loads
   if (loading) {
     return (
       <BlockStack spacing="loose">
@@ -154,19 +151,11 @@ function App1() {
       </BlockStack>
     );
   }
-  // If product variants can't be loaded, then show nothing
+
+
   if (!loading && products.length === 0) {
     return null;
   }
-
-  // Filter out any product variants on offer that are already current cart lines
-  const productsOnOffer = products.filter(
-    (product) => !lines.map((item) => item.merchandise.id).includes(product.id)
-  );
-  // Choose the first available product variant on offer or display the default fallback product variant
-  const { id, img, title, price } = productsOnOffer[0] || products[0];
-  // Localize the currency for international merchants and customers
-  const renderPrice = i18n.formatCurrency(price);
 
   return (
     !(PRODUCT_VARIANTS_DATA1[0].id == ids1 && PRODUCT_VARIANTS_DATA1[1].id == ids2) && 
@@ -195,7 +184,6 @@ function App1() {
                 {PRODUCT_VARIANTS_DATA1[0].title}
               </Text>
               <Text appearance="subdued">{PRODUCT_VARIANTS_DATA1[0].price} 
-              {/* <Icon source="discount"/>1<Icon source="discount"/> */}
               </Text>
             </BlockStack>
 
